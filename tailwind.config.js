0d0cb8d374e26d899099dd859dfeb3f83902cd8b
Harsh Prajapati <prajapati.captain@gmail.com>
2023-07-13 21:38:05 +0530

    fixed something

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{html,js}",
   ],
-
   theme: {
     extend: {},
     fontFamily: {
@@ -13,9 +12,7 @@ export default {
     },
     backgroundImage: {
       "hero-pattern": "url('src/assets/endless.svg')",
-      "footer-texture": "url('/img/footer-texture.png')",
     },
   },
   plugins: [],
-  darkMode: "class",
 };
